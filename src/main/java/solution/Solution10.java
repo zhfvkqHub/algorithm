c73d53f1267baf331682394446631aa1bccae889
kimdayeon <hamoey@naver.com>
2022-10-18 23:44:02 +0900

    2 algorithm add

---
+++ src/main/java/solution/Solution10.java
@@ -0,0 +1,33 @@
+package main.java.solution;
+
+public class Solution10 {
+
+    static String[] player = new String[]{"영수", "광수", "영철", "영기"};
+    public static void main(String[] args) {
+        Solution10 solution10 = new Solution10();
+
+        solution10.playGame(player);
+    }
+
+    public String do369(int number){
+        String num = String.valueOf(number);
+
+        if(num.indexOf("3")>-1 || num.indexOf("6")>-1 || num.indexOf("9")>-1){
+            return ": clap";
+        }else{
+            return ": " + num;
+        }
+
+    }
+    public void playGame(String[] players){
+
+        for(int i=0; i<100;){
+            for(int j=0; j<4; j++){
+                i++;
+                System.out.println(players[j].concat(do369(i)));
+            }
+        }
+
+    }
+
+}
