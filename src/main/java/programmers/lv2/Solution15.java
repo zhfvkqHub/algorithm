3144b7abd44017208b60b3868e5d50534ebfc8c1
kimdayeon <hamoey@naver.com>
2022-11-08 15:38:11 +0900

    다익스트라 알고리즘

---
+++ src/main/java/programmers/lv2/Solution15.java
@@ -0,0 +1,54 @@
+package main.java.programmers.lv2;
+
+// LV2 배달
+public class Solution15 {
+    public static void main(String[] args) {
+        Solution15 solution15 = new Solution15();
+        solution15.solution(5, new int[][]{{1,2,1},{2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2}}, 3);
+
+    }
+
+    public static final int MAXDELIVERY = 500000;
+
+    public int findTarget(int [] d, boolean [] visit) {
+        int target = 0, min = MAXDELIVERY;
+
+        for(int i = 1; i < d.length; i++) {
+            if(d[i] < min && visit[i] == false) {
+                min = d[i];
+                target = i;
+            }
+        }
+        return target;
+    }
+
+    public int solution(int n, int [][] road, int k) {
+        int answer = 0;
+
+        int [] dist = new int[n+1];
+        for(int i = 0; i < dist.length; i++)
+            dist[i] = MAXDELIVERY;
+
+        boolean [] visit = new boolean[n+1];
+        dist[1] = 0;
+
+        for(int i = 0; i < n - 1; i++) {
+            int target = findTarget(dist, visit);
+
+            for(int j = 0; j < road.length; j++) {
+                if(road[j][0] == target || road[j][1] == target) {
+                    int x = road[j][0] == target ? road[j][1] : road[j][0];
+                    if(visit[x] == false) dist[x] = Math.min(dist[x], dist[target] + road[j][2]);
+                }
+            }
+            visit[target] = true;
+        }
+
+        for(int i = 1; i < dist.length; i++) {
+            if(dist[i] <= k) answer++;
+        }
+
+        return answer;
+    }
+
+}
