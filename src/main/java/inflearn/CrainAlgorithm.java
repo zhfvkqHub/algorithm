b2238f8002421cfb110c777db7cab1e05b7efcdf
kimdayeon <hamoey@naver.com>
2023-02-19 22:16:45 +0900

    add algorithm 크레인 인형뽑기

---
+++ src/main/java/inflearn/CrainAlgorithm.java
@@ -0,0 +1,52 @@
+package main.java.inflearn;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CrainAlgorithm {
+
+    public static void main(String[] args) {
+        CrainAlgorithm T = new CrainAlgorithm();
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        
+        for(int i=0; i<n; i++)
+            for (int j=0; j <n; j++)
+                board[i][j] = sc.nextInt();
+
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+
+        for(int i=0; i<m; i++)
+            moves[i] = sc.nextInt();
+
+        System.out.println(T.solution(board, moves));
+        
+    }
+
+    private int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(int pos : moves){
+            for(int i = 0; i< board.length; i++){
+                if(board[i][pos-1] != 0){
+                    int tmp = board[i][pos-1];
+                    board[i][pos-1] = 0;
+                    if(!stack.isEmpty() && tmp == stack.peek()){
+                        answer += 2;
+                        stack.pop();
+                    }else{
+                        stack.push(tmp);
+                    }
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+}
